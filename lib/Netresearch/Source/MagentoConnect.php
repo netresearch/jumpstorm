63ec9a12daf500b71d0b9ddcb26c2d3f857a450f
Christoph Aßmann <christoph.assmann@netresearch.de>
2013-03-07 10:36:53 +0100

    NAFQA-51: fix extension installation via magento connect

--- lib/Netresearch/Source/MagentoConnect.php
+++ lib/Netresearch/Source/MagentoConnect.php
@@ -7,31 +7,48 @@ use Netresearch\Source\Base as Source;
 
 use \Exception as Exception;
 
+/**
+ * Install extension from Magento Connect
+ *
+ * @package    Netresearch
+ * @subpackage Source
+ * @author     Thomas Birke <thomas.birke@netresearch.de>
+ * @author     Christoph Aßmann <christoph.assmann@netresearch.de>
+ */
 class MagentoConnect extends Source implements SourceInterface
 {
     protected $magentoRoot;
 
+    /**
+     * (non-PHPdoc)
+     * @see Netresearch\Source.SourceInterface::copy()
+     */
     public function copy($target, $branch = 'master')
     {
         if (is_null($this->magentoRoot)) {
             throw new Exception("Please provide path to mage shell script (i.e. Magento root directory)");
         }
 
-        $mage = $this->magentoRoot . '/mage';
-        chmod($mage, 0777);
-        exec("$mage mage-setup");
+        chdir($this->magentoRoot);
+        chmod('mage', 0777);
+        exec('./mage mage-setup');
 
         // identifier given: magentoconnect://community/some_key
         // what we need: community some_key
         $identifier = str_replace('/', ' ', substr($this->source, strlen('magentoconnect://')));
 
-        $command = "$mage download $identifier";
+        $command = "./mage download $identifier";
         $response = exec($command);
 
         $path = str_replace('Saved to: ', '', $response);
         exec("mkdir $target; cd $target; tar -xzvf $path");
     }
 
+    /**
+     * Provide the Magento target path in order to locate the mage shell script.
+     *
+     * @param string $path
+     */
     public function setMagentoRoot($path)
     {
         $this->magentoRoot = $path;
